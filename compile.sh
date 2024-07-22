3788ce75f83491c8ea0bf6501e23c9791634e6f7
vanyavorobiov <s19b1_vorobiov@179.ru>
2024-07-22 23:27:37 +0300

    init

---
+++ compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# name of the file
+FILENAME="slides"
+
+# compile.sh once
+pdflatex -interaction=nonstopmode $FILENAME.tex
+
+# delete log files
+rm -f $FILENAME.aux $FILENAME.log $FILENAME.out $FILENAME.bbl $FILENAME.blg $FILENAME.bcf $FILENAME.run.xml $FILENAME.toc
+
+# push changes to the repo
+git add ./*
+git commit -m "auto-commit"
+git push
